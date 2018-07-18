d03012259b9250c6daa9645b361456de8297535a
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-07-17 17:53:14 -0700

    RobotG.java,robotSquare.java

--- src/RobotSquare.java
+++ src/RobotSquare.java
@@ -10,23 +10,26 @@ import org.jointheleague.graphical.robot.Robot;
 public class RobotSquare {
     public static void main(String[] args) throws Exception {
  
-    	// 1. Make a new Robot
-
-
+    	// 1. Make a new robot
+Robot Tatsumi = new Robot("batman");
         // 3. Put the robot's pen down
-
+Tatsumi.penDown();
 
         // 6. Make the robot move as fast as possible
 
 
         // 5. Do everything below here 4 times
+for (int i = 0; i < 4; i++) {
+	
+
+	
 
 
         //         2. Move your robot 200 pixels
-
+Tatsumi.move(200);
 
         //         4. Turn the robot 90 degrees to the right (90 degrees)
-
-
+Tatsumi.turn(90);
+}
     }
 }
