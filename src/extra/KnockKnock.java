7f8028ca01abfa04db04b29548e3b9c6879c6b4b
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-09-25 17:50:20 -0700

    Knock Knock Boi Boi if you dont

---
+++ src/extra/KnockKnock.java
@@ -0,0 +1,21 @@
+package extra;
+
+import javax.swing.JOptionPane;
+
+public class KnockKnock {
+	public static void main(String[] args) {
+		String KirbySucks = JOptionPane.showInputDialog("Knock Knock");
+
+		if (KirbySucks.equals("Whos There")) {
+
+			String Pitisbeast = JOptionPane.showInputDialog("Boi");
+
+			String Boo = JOptionPane.showInputDialog("Boi if you dont");
+			if (Pitisbeast.equals("Boi who")
+		}
+
+			
+
+		
+	}
+}
